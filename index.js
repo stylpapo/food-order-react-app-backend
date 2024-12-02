371b67fc84639bf2fbfd2fd00637d60f981bf7c4
unknown <stelios.papoutsakis@altenar.com>
2024-12-03 01:00:42 +0200

    change port

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   res.send(`Hello ${name}!`);
 });
 
-const port = parseInt(process.env.PORT) || 8080;
+const port = parseInt(process.env.PORT) || 8081;
 app.listen(port, () => {
   console.log(`helloworld: listening on port ${port}`);
 });
